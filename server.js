9f75c917e9f9c55038c62c6a5dd20207b6bc00e3
Kasey Cushing <kasey.cushing@gmail.com>
2021-01-14 18:52:58 -0500

    remove categories model

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const apiRoutes = require("./routes/apiRoutes");
 const express = require("express");
 const session = require("express-session");
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const passport = require("./config/passport");
 
 const db = require("./models");
@@ -14,17 +14,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-require("./routes/apiCategory")(app);
 require("./routes/apiSource")(app);
 require("./routes/apiLoginSignup")(app);
 
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Start server
 db.sequelize.sync({ force: true }).then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
 });
