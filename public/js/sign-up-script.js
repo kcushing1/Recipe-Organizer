68304b5ed0bc43705afc61681544b6519cd58668
JES20 <dore.joel.dore@gmail.com>
2021-01-16 11:50:20 -0500

    - create signup script - update routes in html form

---
+++ public/js/sign-up-script.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    // Grab elements from page
+    const signUpForm = $("#signup");
+    const usernameInput = $("#username");
+    const passwordInput = $("#password");
+    const passwordConfirmInput = $("#password-confirm");
+
+    // Handle form submit
+    signUpForm.on("submit", e => {
+        e.preventDefault();
+
+        // Create user object from inputs
+        const userData = {
+            username: usernameInput.val().trim(),
+            password: passwordInput.val().trim()
+        };
+
+        // Validate inputs aren't empty
+        if (!userData.username || !userData.password) return
+
+        // Validate user password confirmation
+        const passwordConfirm = passwordConfirmInput.val().trim();
+        if (userData.password !== passwordConfirm) {
+            alert("Passwords must match!");
+            passwordInput.val("");
+            passwordConfirmInput.val("");
+            passwordInput.focus();
+            passwordInput.select();
+            return
+        }
+
+        // POST request
+        $.post("/api/signup", userData)
+            .then(() => {
+                window.location.replace("/home");
+            })
+            .catch(err => console.log(err))
+
+        // Clear form
+        usernameInput.val("");
+        passwordInput.val("");
+        passwordConfirmInput.val("");
+    });
+
+
+});
