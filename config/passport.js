739e1c9228bd60ff07c6a20d7ff3ec00c0c98d28
JES20 <dore.joel.dore@gmail.com>
2021-01-16 13:36:06 -0500

    - Fixed errors in passport.js - Reorder app.use statements in server.js

--- config/passport.js
+++ config/passport.js
@@ -13,11 +13,11 @@ passport.use(new LocalStrategy(
         }).then(dbUser => {
             // If username does not match any user in db
             if (!dbUser) {
-                return done(null, false, { message: "Incorrect Username." })
+                return done(null, false, { message: "Incorrect username or password." })
             }
             // If password does not match username in db
             else if (!dbUser.validPassword(password)) {
-                return done(null, false, { message: "Incorrect Password." })
+                return done(null, false, { message: "Incorrect username or password." })
             }
             // Otherwise, return the user from db
             return done(null, dbUser)
