5ef694825afb71f69dbcecb86742deefbafaa7a3
Kasey Cushing <kasey.cushing@gmail.com>
2021-01-17 16:43:56 -0500

    load recipes in nested sidebar, with href to recipe by id - untested

---
+++ public/js/home-script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  //populate the nested navbar with recipes
+  //first, do get request all
+  //then loop over array and append
+  //max number? first ten?
+  function listNavRecipes() {
+    //GET request all recipes
+    $.ajax({
+      type: "GET",
+      url: "/api/recipes",
+    }).then((list) => {
+      console.log(list);
+
+      //loop over list array and append each item to the nested navbar
+      for (let i = 0; i < list.length; i++) {
+        $("#nav-recipe-list").append(`
+        <li class="listing">
+          <a href="/api/recipes/${ilist[i].id}">
+            <h3>${list[i].title}</h3>
+            <p>${list[i].category}</p>
+          </a>
+        </li>
+        `);
+      }
+    });
+  }
+});
