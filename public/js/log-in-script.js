323a978b2e95427f46bc8208d6d4d1e65d4f69a0
Kasey Cushing <kasey.cushing@gmail.com>
2021-01-17 15:58:13 -0500

    git problems

--- public/js/log-in-script.js
+++ public/js/log-in-script.js
@@ -1,31 +1,31 @@
 $(document).ready(() => {
-    // Grab elements from page
-    const loginForm = $("#login");
-    const usernameInput = $("#username");
-    const passwordInput = $("#password");
+  // Grab elements from page
+  const loginForm = $("#login");
+  const usernameInput = $("#username");
+  const passwordInput = $("#password");
 
-    // Handle form submit
-    loginForm.on("submit", e => {
-        e.preventDefault();
+  // Handle form submit
+  loginForm.on("submit", (e) => {
+    e.preventDefault();
 
-        // Create user object from inputs
-        const userData = {
-            username: usernameInput.val().trim(),
-            password: passwordInput.val().trim()
-        };
+    // Create user object from inputs
+    const userData = {
+      username: usernameInput.val().trim(),
+      password: passwordInput.val().trim(),
+    };
 
-        // Validate inputs aren't empty
-        if (!userData.username || !userData.password) return
+    // Validate inputs aren't empty
+    if (!userData.username || !userData.password) return;
 
-        // POST request for login
-        $.post("/api/login", userData)
-            .then(user => {
-                window.location.replace("/home");
-            })
-            .catch(err => console.log(err))
+    // POST request for login
+    $.post("/api/login", userData)
+      .done((user) => {
+        window.location.replace("/home");
+      })
+      .fail((err) => console.log(err));
 
-        // Clear form
-        usernameInput.val("");
-        passwordInput.val("")
-    })
-})
+    // Clear form
+    usernameInput.val("");
+    passwordInput.val("");
+  });
+});
