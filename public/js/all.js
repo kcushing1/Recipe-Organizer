3860d825ccb93c23e7e53200dbd66a033d2cd470
JES20 <dore.joel.dore@gmail.com>
2021-01-20 15:38:40 -0500

    Fix typo in for loop

--- public/js/all.js
+++ public/js/all.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
       type: "GET",
       url: "/api/recipes",
     }).then((list) => {
-      for (let i = 1; i < list.length; i++) {
+      for (let i = 0; i < list.length; i++) {
         $("#load-all-recipes").append(`
       <li><a href="/recipes/${list[i].id}">${list[i].title}</a></li>
       `);
