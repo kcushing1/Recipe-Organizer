b8ca414d1380040712a61bb6635e1c91d33294eb
Kasey Cushing <kasey.cushing@gmail.com>
2021-01-20 15:03:18 -0500

    source title loads to page

--- public/js/template-script.js
+++ public/js/template-script.js
@@ -1,5 +1,6 @@
 loadDishTemplate();
 function loadDishTemplate() {
+  //get recipe id number from url
   function retrieveRecipe() {
     const url = document.URL;
     const dishId = url.split("/").pop();
@@ -19,18 +20,71 @@ function loadDishTemplate() {
       document.querySelector("#show-category").innerText = dish.category;
       document.querySelector("#show-source-url-pg").innerText = dish.url_pg;
       document.querySelector("#show-recipe-notes").innerText = dish.notes;
+
+      $.ajax({
+        type: "GET",
+        url: "/api/sources/" + dish.SourceId,
+      }).then((src) => {
+        document.querySelector("#show-source-title").innerText = src[0].text;
+      });
+
+      //load the number of stars in rating
+      loadStars(dish.rating);
     });
   }
 
-  getSourceName(dishIdNum);
-  function getSourceName(id) {
-    $.ajax({
-      type: "GET",
-      url: "/api/sources/" + id,
-    }).then((resp) => {
-      const name = resp[0].text;
-      console.log("getSourceName " + name);
-      document.querySelector("#show-source-title").innerText = name;
-    });
+  function loadStars(id) {
+    switch (id) {
+      case 0:
+        $("#star-rating").append(`
+          <h4>Uh oh! Looks like this recipe hasn't been tested yet!</h4>
+        `);
+        break;
+      case 1:
+        $("#star-rating").append(`
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star "></span>
+          <span class="fa fa-star "></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+        `);
+        break;
+      case 2:
+        $("#star-rating").append(`
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star "></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+        `);
+        break;
+      case 3:
+        $("#star-rating").append(`
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+        `);
+        break;
+      case 4:
+        $("#star-rating").append(`
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span>
+        `);
+        break;
+      case 5:
+        $("#star-rating").append(`
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+        `);
+        break;
+    }
   }
 }
